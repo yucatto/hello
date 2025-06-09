afc1ae4fa532145c5bbdf508d847f35f5c397c86
yucatto <yukitto0423@gmail.com>
2025-06-09 12:02:05 +0900

    数あてゲーム実装

---
+++ kazuate.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Kazuate {
+    private static int _kazuateNum = 50;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //5回まで試行
+        for(int i = 0; i < 5; i++) {
+            int inputNum = sc.nextInt();
+
+            //当たれば当たり表示を出して終了
+            if(inputNum == _kazuateNum) {
+                System.out.println("当たり");
+                break;
+            }
+
+            //誤差を計算
+            int dif = inputNum - _kazuateNum;
+
+            if(0 < dif) {
+                System.out.println("大きい");
+            } else {
+                System.out.println("小さい");
+            }
+
+            //差が20以上なら
+            if(dif <= -20 || 20 <= dif) {
+                System.out.println("20以上の差があり");
+            }
+        }
+
+        sc.close();
+    }
+}
